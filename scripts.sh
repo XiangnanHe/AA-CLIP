2a770e49cdc7eaa230b24e84a2bca93be92b41e8
shawn_he <xiangnan_he@apple.com>
2025-07-09 09:57:05 -0700

    update data

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,8 @@
 python train.py --save_path ./ckpt/issue --training_mode full_shot
 
 # testing
-declare -a dataset=(MVTec BTAD MPDD Brain Liver Retina Colon_clinicDB Colon_colonDB Colon_Kvasir Colon_cvc300)
+# declare -a dataset=(MVTec BTAD MPDD Brain Liver Retina Colon_clinicDB Colon_colonDB Colon_Kvasir Colon_cvc300)
+declare -a dataset=(MVTec BTAD MPDD)
 save_path="./ckpt/issue"
 for i in "${dataset[@]}"; do
     python test.py --save_path $save_path --dataset $i
